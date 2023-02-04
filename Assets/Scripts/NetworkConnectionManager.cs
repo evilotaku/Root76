686954ee119368abb04444a925ac2815c030ee1a
jmschrack@gmail.com <>
2023-02-04 16:47:54 -0600

    added dontdestoryonload to network connection manager

--- Assets/Scripts/NetworkConnectionManager.cs
+++ Assets/Scripts/NetworkConnectionManager.cs
@@ -43,6 +43,7 @@ public class NetworkConnectionManager : NetworkBehaviour
 
         print($"Logged in as {AuthenticationService.Instance.PlayerId} ");
         VivoxService.Instance.Initialize();
+        DontDestroyOnLoad(this);
     }
 
 
