421933e0031f871223a98dd258f76f0d8aaa1a41
jmschrack@gmail.com <>
2023-02-04 16:58:13 -0600

    Quit button works now

---
+++ Assets/Scripts/UIHelper.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHelper : MonoBehaviour
+{
+   
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
