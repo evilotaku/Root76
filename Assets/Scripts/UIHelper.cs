0f4bb9824b777a0821112342e7d6de890fb960c2
jmschrack@gmail.com <>
2023-02-04 17:50:28 -0600

    Added UI sound effects. Added menu music. Added loading panel to UI.

--- Assets/Scripts/UIHelper.cs
+++ Assets/Scripts/UIHelper.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class UIHelper : MonoBehaviour
 {
-   
-
+    public Image LoadingPanel;
+    public float loadPanelFadeInTime=0.5f;
+    public void ShowLoadingScreen(){
+        StartCoroutine(_showLoadingScreen());
+    }
+    IEnumerator _showLoadingScreen(){
+        LoadingPanel.gameObject.SetActive(true);
+        float elapsedTime=0f;
+        Color startColor=LoadingPanel.color;
+        while(elapsedTime<=loadPanelFadeInTime){
+            startColor.a=Mathf.Lerp(0,1,elapsedTime/loadPanelFadeInTime);
+            LoadingPanel.color=startColor;
+            yield return 0;
+            elapsedTime+=Time.deltaTime;
+        }
+    }
     public void QuitGame(){
         Application.Quit();
     }
