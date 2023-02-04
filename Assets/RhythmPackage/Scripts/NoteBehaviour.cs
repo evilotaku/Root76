d742322381cc3ebb8f0f33478005fa388381cd00
evilotaku@gmail.com <>
2023-02-04 12:50:35 -0600

    made rhythm package into UI canvas

--- Assets/RhythmPackage/Scripts/NoteBehaviour.cs
+++ Assets/RhythmPackage/Scripts/NoteBehaviour.cs
@@ -13,12 +13,12 @@ public class NoteBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = new Vector2(-speed,0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        rb.velocity = new Vector2(-speed, 0);
     }
 }
